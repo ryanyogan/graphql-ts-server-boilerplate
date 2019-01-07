85e2f82e2b8879cad66d6ab608c1868d7e2a82bb
Ryan Yogan <ryanyogan@gmail.com>
2019-01-07 13:04:44 -0800

    basics of jest setup, needs better config

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,13 @@ import * as path from "path";
 
 import { resolvers } from "./resolvers";
 
-const typeDefs = importSchema(path.join(__dirname, "./schema.graphql"));
+export const startServer = async () => {
+  const typeDefs = importSchema(path.join(__dirname, "./schema.graphql"));
 
-const server = new GraphQLServer({ typeDefs, resolvers });
+  const server = new GraphQLServer({ typeDefs, resolvers });
+  await createConnection();
+  await server.start();
+  console.log("Server is running on http://localhost:4000");
+};
 
-createConnection().then(() => {
-  server.start(() => console.log("Server is running on http://localhost:4000"));
-});
+startServer();
