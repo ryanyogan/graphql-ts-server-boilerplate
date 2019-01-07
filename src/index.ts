439b6c5f0496fab8013342376da5210171137e12
Ryan Yogan <ryanyogan@gmail.com>
2019-01-07 11:55:46 -0800

    created types based on Gql schema

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,9 @@
 import "reflect-metadata";
-
+import { importSchema } from "graphql-import";
 import { GraphQLServer } from "graphql-yoga";
+import { resolvers } from "./resolvers";
 
-const typeDefs = `
-  type Query {
-    hello(name: String): String!
-  }
-`;
-
-const resolvers = {
-  Query: {
-    hello: (_: any, { name }: any) => `Hello ${name || "World"}`
-  }
-};
+const typeDefs = importSchema("schema.graphql");
 
 const server = new GraphQLServer({ typeDefs, resolvers });
 server.start(() => console.log("Server is running on http://localhost:4000"));
