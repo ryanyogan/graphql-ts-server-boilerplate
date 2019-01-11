94b7cde810d4ec4155c2fb44aecc6dc1d8234c9b
Ryan Yogan <ryanyogan@gmail.com>
2019-01-10 20:56:13 -0500

    added logout module, fixed tests to pass (sequential for now)

--- src/modules/logout/__tests__/logout.test.ts
+++ src/modules/logout/__tests__/logout.test.ts
@@ -3,8 +3,8 @@ import { createTypeormConnection } from "../../../utils/createTypeormConnection"
 import { User } from "../../../entity/User";
 import { Connection } from "typeorm";
 
-let conn: Connection;
 let userId: string;
+let conn: Connection;
 const email = "bob5@bob.com";
 const password = "jlkajoioiqwe";
 
@@ -17,7 +17,13 @@ const loginMutation = (e: string, p: string) => `
   }
 `;
 
-export const meQuery = `
+const logoutMutation = `
+  mutation {
+    logout
+  }
+`;
+
+const meQuery = `
   {
     me {
       id
@@ -40,15 +46,8 @@ afterAll(async () => {
   conn.close();
 });
 
-describe("me", () => {
-  it("returns null if there is no cookie", async () => {
-    const response = await axios.post(process.env.TEST_HOST as string, {
-      query: meQuery
-    });
-    expect(response.data.data.me).toBeNull();
-  });
-
-  it("retreives current user ", async () => {
+describe("logout", () => {
+  it("logout current user", async () => {
     await axios.post(
       process.env.TEST_HOST as string,
       {
@@ -59,7 +58,7 @@ describe("me", () => {
       }
     );
 
-    const response = await axios.post(
+    const response1 = await axios.post(
       process.env.TEST_HOST as string,
       {
         query: meQuery
@@ -69,11 +68,28 @@ describe("me", () => {
       }
     );
 
-    expect(response.data.data).toEqual({
+    expect(response1.data.data).toEqual({
       me: {
         id: userId,
         email
       }
     });
+
+    await axios.post(
+      process.env.TEST_HOST as string,
+      {
+        query: logoutMutation
+      },
+      { withCredentials: true }
+    );
+
+    const response = await axios.post(
+      process.env.TEST_HOST as string,
+      {
+        query: meQuery
+      },
+      { withCredentials: true }
+    );
+    expect(response.data.data.me).toBeNull();
   });
 });
