638a9139d31a38c5f44e2e1075edaeed2fcc5c14
Ryan Yogan <ryanyogan@gmail.com>
2019-01-10 19:17:27 -0500

    fixed me failing tests

--- src/modules/me/resolvers.ts
+++ src/modules/me/resolvers.ts
@@ -1,13 +1,12 @@
 import { ResolverMap } from "../../types/graphql-utils";
-// import { User } from "../../entity/User";
+import { User } from "../../entity/User";
 import { createMiddleware } from "../../utils/createMiddleware";
 import middleware from "./middleware";
 
 export const resolvers: ResolverMap = {
   Query: {
-    me: createMiddleware(middleware, (_, __, { session }) => {
-      throw new Error("fuck" + JSON.stringify(session));
-      // User.findOne({ where: { id: session.userId } })
-    })
+    me: createMiddleware(middleware, (_, __, { session }) =>
+      User.findOne({ where: { id: session.userId } })
+    )
   }
 };
